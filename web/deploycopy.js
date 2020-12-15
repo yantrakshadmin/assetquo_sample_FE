bcd2041786e7e6b65420f804b7e883a65fcf32e0
Rahul Sharma <yantralogistics@gmail.com>
2020-12-15 18:21:17 +0530

    Basic Data

--- web/deploycopy.js
+++ web/deploycopy.js
@@ -3,16 +3,16 @@ const path = require('path');
 
 const indexFile = path.join('build', 'index.html');
 const error404File = path.join('build', '404.html');
-// const cnameOriginal = path.join('CNAME');
-// const cnameBuild = path.join('build', 'CNAME');
+const cnameOriginal = path.join('CNAME');
+const cnameBuild = path.join('build', 'CNAME');
 
 fs.copyFile(indexFile, error404File, (err) => {
   if (err) throw err;
   console.log('build/index.html was copied to build/404.html');
 });
 
-// fs.copyFile(cnameOriginal, cnameBuild, (err) => {
-//   if (err) throw err;
-//   console.log('CNAME was copied to build/CNAME');
-// });
+fs.copyFile(cnameOriginal, cnameBuild, (err) => {
+  if (err) throw err;
+  console.log('CNAME was copied to build/CNAME');
+});
 // push
